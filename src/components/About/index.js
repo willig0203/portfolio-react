959a6c4925e31791fd048597f478db3ac9f4c2e0
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-11-15 13:56:40 -0600

    about changes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,13 +7,12 @@ function About() {
       <div className="about-text">
         <ul>
           <li>
-            An aspiring Full Stack Web Developer with extensive knowledge of
-            software systems and a background working with an automation group
-            in a plant manufacturing environment, interacting with all
-            departments at problem solving and innovation, saving time and
-            reducing labor costs, adding transparency in information and
-            reporting, creating very impressive software that is relied upon and
-            still in use today.
+            A Full Stack Web Developer with extensive knowledge of software
+            systems and a background working with an automation group in a plant
+            manufacturing environment, interacting with all departments at
+            problem solving and innovation, saving time and reducing labor
+            costs, adding transparency in information and reporting, creating
+            very impressive software that is relied upon and still in use today.
           </li>
         </ul>
       </div>
